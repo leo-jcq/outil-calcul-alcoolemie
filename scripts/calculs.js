de76d7aeaa691ac6b0f9889f91f35efee6ea2d09
Jacquet Léo <leojacquet2003@gmail.com>
2022-08-22 17:19:39 +0200

    Fonction de calcul finie

--- scripts/calculs.js
+++ scripts/calculs.js
@@ -4,16 +4,15 @@ function calculAlcoolemie() {
     let sexe = document.getElementById("sexeSelector");
     let poid = document.getElementById("poidInput");
     let temps = document.getElementById("tempsInput");
-    let permis = document.getElementById("permisButton");
+    let permis = document.getElementById("permis");
 
     // calcul de l'alcoolémie
     let alcoolemie = ((nbVerres.value * 10) / (poid.value * sexe.value)) - ((temps.value - 0.5) * poid.value * 0.002);
     if (alcoolemie < 0) {
-        alcoolemie = 0
+        alcoolemie = 0;
     } else {
         alcoolemie = alcoolemie.toFixed(2);
     }
-
 
     // affichage de l'alcoolémie
     let taux = document.getElementById("taux");
@@ -21,17 +20,17 @@ function calculAlcoolemie() {
 
     // condition pour prendre le volant
     let volant = document.getElementById("volant");
-    if ((!permis.checked && alcoolemie < 0.5)) {
+    if (!permis.checked) {
         if (alcoolemie < 0.5) {
             volant.innerHTML = "Vous pouvez prendre le volant !";
-        } else {
-            volant.innerHTML = "Votre taux d'alcoolémie est trop élevé pour prendre le volant !"
+        } else{
+            volant.innerHTML = "Votre taux d'alcoolémie est trop élevé pour prendre le volant !";
         }
     } else {
         if (alcoolemie < 0.2) {
             volant.innerHTML = "Vous pouvez prendre le volant !";
-        } else {
-            volant.innerHTML = "Votre taux d'alcoolémie est trop élevé pour prendre le volant !"
+        } else{
+            volant.innerHTML = "Votre taux d'alcoolémie est trop élevé pour prendre le volant !";
         }
     }
 }
